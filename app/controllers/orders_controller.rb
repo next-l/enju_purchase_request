4793bc9e6c01de025b89765d717ee61d49ae9170
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-10-20 20:55:10 +0900

    removed check_client_ip_address

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,4 @@
 class OrdersController < ApplicationController
-  before_filter :check_client_ip_address
   load_and_authorize_resource
   before_filter :get_order_list
   before_filter :get_purchase_request
