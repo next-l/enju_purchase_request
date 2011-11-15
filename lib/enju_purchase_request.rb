c4624a80fa13dd073db729837885145eab0ea431
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2011-11-15 10:46:31 +0900

    removed user

--- lib/enju_purchase_request.rb
+++ lib/enju_purchase_request.rb
@@ -1,6 +1,5 @@
 require "enju_purchase_request/engine"
 require "enju_purchase_request/bookstore"
-require "enju_purchase_request/user"
 
 module EnjuPurchaseRequest
 end
--- lib/enju_purchase_request/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ActiveRecord::Base
-  has_many :purchase_requests
-  has_many :order_lists
-end
