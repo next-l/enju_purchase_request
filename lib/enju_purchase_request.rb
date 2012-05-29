22425c33b243469b7f835db558de964c6b93af0d
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-05-30 04:32:50 +0900

    moved some methods

--- lib/enju_purchase_request.rb
+++ lib/enju_purchase_request.rb
@@ -4,4 +4,27 @@ require "enju_purchase_request/url_validator"
 require "enju_purchase_request/bookmark_url"
 
 module EnjuPurchaseRequest
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def enju_subject
+      include EnjuPurchaseRequest::InstanceMethods
+    end
+  end
+
+  module InstanceMethods
+    private
+
+    def get_order_list
+      @order_list = OrderList.find(params[:order_list_id]) if params[:order_list_id]
+    end
+
+    def get_purchase_request
+      @purchase_request = PurchaseRequest.find(params[:purchase_request_id]) if params[:purchase_request_id]
+    end
+  end
 end
+
+ActionController::Base.send(:include, EnjuPurchaseRequest)
