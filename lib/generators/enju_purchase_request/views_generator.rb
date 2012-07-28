092871296737277aba43c6863fdfae13ab81b79c
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2012-07-29 00:47:45 +0900

    added generator

---
+++ lib/generators/enju_purchase_request/views_generator.rb
@@ -0,0 +1,19 @@
+module EnjuPurchaseRequest
+  module Generators
+    class ViewsGenerator < Rails::Generators::Base
+      source_root File.expand_path('../../../../app/views', __FILE__)
+
+      def copy_files
+        directories = %w(
+          order_lists
+          orders
+          purchase_requests
+        )
+
+        directories.each do |dir|
+          directory dir, "app/views/#{dir}"
+        end
+      end
+    end
+  end
+end
