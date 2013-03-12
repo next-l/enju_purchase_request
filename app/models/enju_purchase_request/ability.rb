a0a6af7f5a769098da2ecb572c9a7574990f3f13
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-03-12 17:15:32 +0900

    added ability module

---
+++ app/models/enju_purchase_request/ability.rb
@@ -0,0 +1,30 @@
+module EnjuPurchaseRequest
+  class Ability
+    include CanCan::Ability
+  
+    def initialize(user, ip_address = nil)
+      case user.try(:role).try(:name)
+      when 'Administrator'
+        can :destroy, Bookstore do |bookstore|
+          bookstore.order_lists.empty? and bookstore.items.empty?
+        end
+        can :manage, [
+          Order,
+          OrderList,
+          PurchaseRequest
+        ]
+      when 'Librarian'
+        can :manage, [
+          Order,
+          OrderList,
+          PurchaseRequest
+        ]
+      when 'User'
+        can [:index, :create], PurchaseRequest
+        can [:show, :update, :destroy], PurchaseRequest do |purchase_request|
+          purchase_request.user == user
+        end
+      end
+    end
+  end
+end
