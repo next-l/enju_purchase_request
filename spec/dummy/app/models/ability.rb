033a4519085cdf01691ed762fba4b7451a393143
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-03-13 11:30:45 +0900

    updated ability file

--- spec/dummy/app/models/ability.rb
+++ spec/dummy/app/models/ability.rb
@@ -1,25 +1,30 @@
-class Ability
-  include CanCan::Ability
-
-  def initialize(user)
-    case user.try(:role).try(:name)
-    when 'Administrator'
-      can :manage, [
-        Order,
-        OrderList,
-        PurchaseRequest
-      ]
-    when 'Librarian'
-      can :manage, [
-        Order,
-        OrderList,
-        PurchaseRequest
-      ]
-    when 'User'
-      can [:index, :create], PurchaseRequest
-      can [:show, :update, :destroy], PurchaseRequest do |purchase_request|
-        purchase_request.user == user
+#module EnjuPurchaseRequest
+  class Ability
+    include CanCan::Ability
+  
+    def initialize(user, ip_address = nil)
+      case user.try(:role).try(:name)
+      when 'Administrator'
+        can :destroy, Bookstore do |bookstore|
+          bookstore.order_lists.empty? and bookstore.items.empty?
+        end
+        can :manage, [
+          Order,
+          OrderList,
+          PurchaseRequest
+        ]
+      when 'Librarian'
+        can :manage, [
+          Order,
+          OrderList,
+          PurchaseRequest
+        ]
+      when 'User'
+        can [:index, :create], PurchaseRequest
+        can [:show, :update, :destroy], PurchaseRequest do |purchase_request|
+          purchase_request.user == user
+        end
       end
     end
   end
-end
+#end
