03563fb1f8a84bab2084942d9deeb904fb40c118
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-04-12 01:03:48 +0900

    added generator

---
+++ lib/generators/enju_purchase_request/setup/setup_generator.rb
@@ -0,0 +1,8 @@
+class EnjuPurchaseRequest::SetupGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+
+  def setup
+    rake("enju_purchase_request_engine:install:migrations")
+    inject_into_file 'app/models/user.rb',
+      "  enju_purchase_request_user_model\n", :after => "enju_leaf_user_model\n"
+end
