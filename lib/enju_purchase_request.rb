2563eda8a6c6f09dbed5480bee00bd5857af74cb
Kosuke Tanabe <nabeta@fastmail.fm>
2015-12-08 20:01:48 +0900

    show order_list when logged in as Librarian next-l/enju_leaf#925

--- lib/enju_purchase_request.rb
+++ lib/enju_purchase_request.rb
@@ -17,11 +17,17 @@ module EnjuPurchaseRequest
     private
 
     def get_order_list
-      @order_list = OrderList.find(params[:order_list_id]) if params[:order_list_id]
+      if params[:order_list_id]
+        @order_list = OrderList.find(params[:order_list_id])
+        authorize @order_list, :show?
+      end
     end
 
     def get_purchase_request
-      @purchase_request = PurchaseRequest.find(params[:purchase_request_id]) if params[:purchase_request_id]
+      if params[:purchase_request_id]
+        @purchase_request = PurchaseRequest.find(params[:purchase_request_id])
+        authorize @purchase_request,  :show?
+      end
     end
   end
 end
