6aa841043ca7ade3e7e9199857aa91104389d158
Kosuke Tanabe <nabeta@fastmail.fm>
2018-12-16 01:05:31 +0900

    require factory_bot

--- spec/factories/purchase_request.rb
+++ spec/factories/purchase_request.rb
@@ -1,6 +1,6 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :purchase_request do |f|
     f.sequence(:title){|n| "purchase_request_#{n}"}
-    f.user_id{FactoryGirl.create(:user).id}
+    f.user_id{FactoryBot.create(:user).id}
   end
 end
